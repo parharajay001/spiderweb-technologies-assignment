12544062eab0c5ca1f3c1a1cfd00c6862690b8f0
Ajay Parhar <parharajay001@gmail.com>
2025-06-01 22:32:13 +0530

    Updates

--- src/assets/icons/index.ts
+++
@@ -1,5 +0,0 @@
-const Logo = new URL('./logo.svg', import.meta.url).href;
-const BellIcon = new URL('./bell_icon.svg', import.meta.url).href;
-const InfoIcon = new URL('./info_icon.svg', import.meta.url).href;
-
-export { Logo, BellIcon, InfoIcon };
--- src/assets/images/index.ts
+++
@@ -1,3 +0,0 @@
-const ProfileImage = new URL('./profile_image.jpg', import.meta.url).href;
-
-export { ProfileImage };
